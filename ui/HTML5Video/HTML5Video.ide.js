87bec653f1028c8a36290ad14e6b5c584826280e
Lacatus <placatus@ptc.com>
2017-04-19 14:01:11 +0300

    Code formatting

--- ui/HTML5Video/HTML5Video.ide.js
+++ ui/HTML5Video/HTML5Video.ide.js
@@ -1,60 +1,60 @@
 TW.IDE.Widgets.HTML5Video = function () {
 	this.widgetProperties = function () {
 		return {
-			'name' : 'HTML5Video',
-			'description' : 'Enables playback of MP4 files.',
-			'category' : ['Common'],
-            'isResizable': true,
+			'name': 'HTML5Video',
+			'description': 'Enables playback of MP4 files.',
+			'category': ['Common'],
+			'isResizable': true,
 			'supportsAutoResize': true,
 			'supportsLabel': true,
-			'isExtension':true,
-			'properties' : {
-					'VideoLocation' : {
-					'baseType' : 'STRING',
-					'defaultValue' : 'http:',
+			'isExtension': true,
+			'properties': {
+				'VideoLocation': {
+					'baseType': 'STRING',
+					'defaultValue': 'http:',
 					'isBindingTarget': true,
 					'isVisible': true,
 					'warnIfNotBoundAsSource': true
 				},
 				'AutoPlay': {
-                    'baseType' : 'BOOLEAN',
+					'baseType': 'BOOLEAN',
 					'isVisible': true,
-					'defaultValue' : true,
-                },
-//				'FitMethod': {
- //                   'baseType': 'STRING',
- //                   'defaultValue': 'fittowidth',
- //                   'selectOptions': [
- //                       { value: 'fittowidth', text: 'Fit to Width' },
- //                       { value: 'fittoheight', text: 'Fit to Height' }
- //                   ]
- //               },
+					'defaultValue': true,
+				},
+				//				'FitMethod': {
+				//                   'baseType': 'STRING',
+				//                   'defaultValue': 'fittowidth',
+				//                   'selectOptions': [
+				//                       { value: 'fittowidth', text: 'Fit to Width' },
+				//                       { value: 'fittoheight', text: 'Fit to Height' }
+				//                   ]
+				//               },
 				'ShowControls': {
-                    'baseType' : 'BOOLEAN',
+					'baseType': 'BOOLEAN',
 					'isVisible': true,
-					'defaultValue' : true,
-                }
+					'defaultValue': true,
+				}
 			}
 		};
 	};
-	 this.widgetServices = function () {
-        return {
-        };
-    };
-	 this.renderHtml = function () {
-	
-        var html = '';
-        html += '<div class="widget-content widget-HTML5Video">';
-        html += '<span>HTML5Video here</span>';
-        html += '</div>';
-        return html;
-    };
-	
+	this.widgetServices = function () {
+		return {
+		};
+	};
+	this.renderHtml = function () {
+
+		var html = '';
+		html += '<div class="widget-content widget-HTML5Video">';
+		html += '<span>HTML5Video here</span>';
+		html += '</div>';
+		return html;
+	};
+
 	this.afterRender = function () {
-};
-	
-	    this.afterSetProperty = function (name, value) {
+	};
+
+	this.afterSetProperty = function (name, value) {
 		return false;
-    };
+	};
 
 };
--- ui/HTML5Video/HTML5Video.runtime.js
+++ ui/HTML5Video/HTML5Video.runtime.js
@@ -1,74 +1,71 @@
 TW.Runtime.Widgets.HTML5Video = function () {
 	var valueElem;
 	var htmlToWrite = '';
-	
-	
-	  this.runtimeProperties = function () {
-	        return {
-                'supportsAutoResize': true
-	        };
-	    };
-		
+
+
+	this.runtimeProperties = function () {
+		return {
+			'supportsAutoResize': true
+		};
+	};
+
 	this.renderHtml = function () {
-		var html='<div class="widget-content widget-HTML5Video">' +
-	        				'<div class="widget-HTML5Video-container ">' +
-		'<video '+this.GetAutoPlay()+' '+this.GetShowControls()+' '+this.GetFitMethod()+' >' +
-		'<source src=" '  +this.getProperty('VideoLocation')+'" type="video/mp4">' +
-		'Your browser does not support the video tag.' +
-		'</video> ' +
-		
-		'</div></div>';
+		var html = '<div class="widget-content widget-HTML5Video">' +
+			'<div class="widget-HTML5Video-container ">' +
+			'<video ' + this.GetAutoPlay() + ' ' + this.GetShowControls() + ' ' + this.GetFitMethod() + ' >' +
+			'<source src=" ' + this.getProperty('VideoLocation') + '" type="video/mp4">' +
+			'Your browser does not support the video tag.' +
+			'</video> ' +
+
+			'</div></div>';
 		return html;
 	};
-	this.GetAutoPlay = function()
-	{
-		if (this.getProperty('AutoPlay')==true)
-		return "autoplay";
+	this.GetAutoPlay = function () {
+		if (this.getProperty('AutoPlay') == true)
+			return "autoplay";
 		else return "";
 	};
-	
-	this.GetShowControls = function()
-	{
-		if (this.getProperty('ShowControls')==true)
-		return "controls";
+
+	this.GetShowControls = function () {
+		if (this.getProperty('ShowControls') == true)
+			return "controls";
 		else return "";
 	}
-	
-	this.GetFitMethod = function()
-	{
-/* 		if (this.getProperty('FitMethod') == 'fittowidth')
-		{
-			return "width=\"100%\"";
-		}
-		else if (this.getProperty('FitMethod') == 'fittoheight')
-		{
-			return "height=\"100%\"";
-		} */
+
+	this.GetFitMethod = function () {
+		/* 		if (this.getProperty('FitMethod') == 'fittowidth')
+				{
+					return "width=\"100%\"";
+				}
+				else if (this.getProperty('FitMethod') == 'fittoheight')
+				{
+					return "height=\"100%\"";
+				} */
 		return "height=\"100%\" width=\"100%\"";
 	}
-	
+
 	this.afterRender = function () {
 	};
 
 	this.updateProperty = function (updatePropertyInfo) {
 		var widgetElement = this.jqElement.find("source");
-		
+
 		// TargetProperty tells you which of your bound properties changed
 		if (updatePropertyInfo.TargetProperty === 'VideoLocation') {
-			
+
 			var srcVideo = updatePropertyInfo.SinglePropertyValue;
-			
+
 			this.setProperty('VideoLocation', srcVideo);
-			
-			widgetElement.attr("src",srcVideo);
+
+			widgetElement.attr("src", srcVideo);
 			var videoPlayer = this.jqElement.find("video");
 			videoPlayer.trigger('load');
-			if (this.getProperty('AutoPlay')==true)
+			if (this.getProperty('AutoPlay') == true)
 				videoPlayer.trigger('play');
-			
-			
-			
-			
+
+
+
+
 		}
 	};
 };
